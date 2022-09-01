e498004ba74f56a953454f3a3c077093c8c3edb4
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2022-09-01 10:13:58 +0200

    modif du nom de la societe et transformations des messsage reçus par mail en français

--- back_end/utils/sendMail.js
+++ back_end/utils/sendMail.js
@@ -33,50 +33,50 @@ const send = async (info) => {
 export const emailProcess = ({ email, pin, type, verificationLink = "" }) => {
   let info = "";
   switch (type) {
-    case "request-new-password":
+    case "recup-code-pin":
       info = {
-        from: '"CMR Company" <shyann.paucek42@ethereal.email>', //expéditeur
-        to: email, // destinataire
-        subject: "Password rest Pin", // Objet du mail
+        from: '"KSTransport <shyann.paucek42@ethereal.email>', //expéditeur
+        to: email, // destinataires
+        subject: "Code pine de réinitialisation du mot de passe", // Objet du mail
         text:
-          "Here is your password rest pin" +
+          "Bonjour, voici le Code pine de réinitialisation du mot de passe" +
           pin +
-          " This pin will expires in 1day", // message en format text brut
-        html: `<b>Hello </b>
-      Here is your pin 
+          " Ce code expirera en un jour", // message en format text brut
+        html: `<b>Bonjour </b>
+        voici le Code pine de réinitialisation du mot de passe
       <b>${pin} </b>
-      This pin will expires in 1day
+      " Ce code expirera dans un jour
       <p></p>`, // message en format html
       };
 
       send(info);
       break;
 
-    case "update-password-success":
+    case "modification-mot-de-passe":
       info = {
-        from: '"CMR Company" <shyann.paucek42@ethereal.email>', //expéditeur
-        to: email, // list of receivers
-        subject: "Password updated", // Subject line
-        text: "Your new password has been update", // plain text body
-        html: `<b>Hello </b>
+        from: '"KSTransport <shyann.paucek42@ethereal.email>', //expéditeur
+        to: email, // destinataire
+        subject: "Modification mot de passe", // Objet du mail
+        text: "Bonjour, votre mot de passe à été modifié avec succès", // message en texte brut
+        html: `<b>Bonjour </b>
        
-      <p>Your new password has been update</p>`, // html body
+      <p>votre mot de passe à été modifié avec succès</p>`, // message en format html
       };
 
       send(info);
       break;
 
-    case "new-user-confirmation-required":
-      info = {
-        from: '"CMR Company"<shyann.paucek42@ethereal.email>', //expéditeur
-        to: email, // list of receivers
-        subject: "Please verify your new user", // Subject line
-        text: "Please follow the link to very your account before you can login", // plain text body
-        html: `<b>Hello </b>
-        <p>Please follow the link to very your account before you can login</p>
-        <p>${verificationLink}</P>
-        `, // html body
-      };
+      // case "new-user-confirmation-required":
+      //   info = {
+      //     from: '"KSTransport <shyann.paucek42@ethereal.email>', //expéditeur
+      //     to: email, // destinataires
+      //     subject: "Please verify your new user", // sujet du message
+      //     text: "Please follow the link to very your account before you can login", // message en texte brut
+      //     html: `<b>Hello </b>
+      //     <p>Please follow the link to very your account before you can login</p>
+      //     <p>${verificationLink}</P>
+      //     `, // version html du message
+      //   };
 
       send(info);
       break;
