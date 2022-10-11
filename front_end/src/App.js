4a4a13e3c1c3b908a5ad419dabbf3bffe4a68248
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2022-10-11 10:10:37 +0200

    ajout de la fonction qui retourne la page courante au rechargement de la page et ajout du composant de chargement

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/home/Home.page.js";
 import InscriptionPage from "./Pages/inscription/Inscription.page.js";
@@ -8,13 +9,28 @@ import AddTicket from "./Pages/addTicket/AddTicket.page";
 import Ticket from "./Pages/ticket/Ticket.page";
 import ListTicket from "./Pages/listeTicket/ListTicket.page";
 import DefaultLayout from "./layout/DefaultLayout.js";
+import Chargement from "./Pages/logout/Chargement.js";
 import RecupPinPage from "./Pages/password-rest/RecupPinPage";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserProfil, getNewAccesUserProfil } from "./Actions/userAction.js";
+
 import "./Styles/App.css";
-// import { getNewAccesUserProfil, getUserProfil } from "./Actions/userAction.js";
+
 // Ce composant contient toute la logique de notre application
 function App() {
   const { user, isConnect } = useSelector((state) => state.userReducer);
+
+  const dispatch = useDispatch();
+
+  // la fnction qui sera lancé quand on raffraichir la
+  useEffect(() => {
+    !user._id && dispatch(getUserProfil());
+
+    !sessionStorage.getItem("token") &&
+      !isConnect &&
+      localStorage.getItem("token") &&
+      dispatch(getNewAccesUserProfil());
+  }, [dispatch, isConnect, user._id]);
 
   return (
     <div className="App">
@@ -38,29 +54,31 @@ function App() {
           </Route>
           {/* creation de la mise en page de nos composant route*/}
           {user && user._id && isConnect && (
-            <DefaultLayout>
-              {/* route pour acceder au tableau de bord*/}
-              <Route exact path="/dashboard">
-                <Dashboard />
-              </Route>
-              {/* route pour acceder à la page d'ajout du ticket */}
-              <Route exact path="/ajout-ticket">
-                <AddTicket />
-              </Route>
-              {/* route pour acceder à la page de visualisation d'un ticket*/}
-              <Route exact path="/ticket/:tId">
-                <Ticket />
-              </Route>
-              {/* route pour acceder à la liste des ticket */}
-              <Route exact path="/list-tickets">
-                <ListTicket />
-              </Route>
-            </DefaultLayout>
+            <div>
+              <DefaultLayout>
+                {/* route pour acceder au tableau de bord*/}
+                <Route exact path="/dashboard">
+                  <Dashboard />
+                </Route>
+                {/* route pour acceder à la page d'ajout du ticket */}
+                <Route exact path="/ajout-ticket">
+                  <AddTicket />
+                </Route>
+                {/* route pour acceder à la page de visualisation d'un ticket*/}
+                <Route exact path="/ticket/:tId">
+                  <Ticket />
+                </Route>
+                {/* route pour acceder à la liste des ticket */}
+                <Route exact path="/list-tickets">
+                  <ListTicket />
+                </Route>
+              </DefaultLayout>
+            </div>
           )}
-
           <Route path="*">
-            <h1>404 Page not found</h1>
+            <Chargement />
           </Route>
+          {/* )} */}
         </Switch>
       </Router>
     </div>
