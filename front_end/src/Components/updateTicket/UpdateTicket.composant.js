665a45fd30745938f205f980ffa9c6401b7b261f
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2022-12-05 17:04:41 +0100

    champ et methode d'ajout d'image dans la discussion via le formulaire

--- front_end/src/Components/updateTicket/UpdateTicket.composant.js
+++ front_end/src/Components/updateTicket/UpdateTicket.composant.js
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 import { Form, Button } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  getSingleTicketInfoByAdmin,
-  ResponseTicket,
+  getSingleTicketInfo,
   updateStatusResponse,
 } from "../../Actions/ticketAction.js";
+import { REPLY_TICKETS } from "../../Actions/ticketAction.js";
 
 // ce composant retourne le formulaire pour reponse a un ticket
 const UpdateTicket = ({ _id }) => {
   //on recupere les info sur le user connecté
   const { user } = useSelector((state) => state.userReducer);
   console.log(user.role);
-
-  // on recupere les données du ticket du user connecté
-  const { selectedTicket } = useSelector((state) => state.ticketReducer);
-  // console.log(selectedTicket._id);
 
   // la variable qui dechenche une action
   const dispatch = useDispatch();
@@ -27,54 +24,116 @@ const UpdateTicket = ({ _id }) => {
 
   // la variable qui contient la reponse de l'expéditeur
   const [message, setMessage] = useState("");
+  // la variable qui contient le fichier image de l'expediteur
+  const [file, setFile] = useState("");
 
-  //fonction qui met à jour le champ de connexion
+  // Fonction asynchrone qui permet de repondre a un ticket
+  const getResponseTicketApi = async (data) => {
+    if (_id !== null) {
+      const ticketId = _id;
+      await axios({
+        method: "put",
+        url: `${process.env.REACT_APP_API_URL}api/ticket/${ticketId}`,
+        withCredentials: true,
+        headers: {
+          Authorization: sessionStorage.getItem("token"),
+          "Content-Type": "multipart/form-data",
+        },
+        // on lui passe les data pour repondre a un ticket
+        data: data,
+      })
+        .then((res) => {
+          console.log(res.data);
+          // on dispact le type REPLY_TICKETS pour recuperer les info du payload
+          // et le stocker dans le reducer
+          dispatch({
+            type: REPLY_TICKETS,
+            payload: res.data,
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+    dispatch(getSingleTicketInfo(_id));
+    // pour vider le champs input message
+    setMessage("");
+    // pour vider le champs input file
+    setFile("");
+    // fonction pour vider le formulaire après soumission
+    // le mot form est le name dans le formulaire
+    await document.form.reset();
+  };
+
+  // fonction qui permet de changer le contenu de l'input
+  const handleFileInput = (event) => {
+    const fileObj = event.target.files && event.target.files[0];
+    console.log(fileObj);
+    setFile(fileObj);
+
+    if (!fileObj) {
+      return;
+    }
+
+    console.log("fileObj is", fileObj);
+  };
+
+  // console.log("fichier image:", file.name);
+
+  //fonction qui met à jour le contenu du champ de message
   const handleOnChange = (e) => {
     setMessage(e.target.value);
     console.log("message:" + message);
   };
 
   // la fonction qui sera exécuté lorsque le formulaire sera soumis
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
+
+    //on soumet pas le message à l'abscence d'une reponse
+    if (!message) {
+      return;
+    }
+
+    // variable contenant notre image
+    // ainsi que les informations liée a l'image
+    const data = new FormData();
+    // les informations liée à l'image
+    data.append("message", message);
+    data.append("expediteur", expediteur);
+
+    if (file) {
+      data.append("image", file);
+    }
+
     if (user.role === "client") {
-      message &&
-        expediteur &&
-        dispatch(ResponseTicket(_id, message, expediteur));
-      dispatch(getSingleTicketInfoByAdmin(selectedTicket._id));
-      setMessage("");
+      await getResponseTicketApi(data);
     }
 
     if (user.role === "admin") {
-      message &&
-        expediteur &&
-        dispatch(ResponseTicket(_id, message, expediteur));
-      dispatch(getSingleTicketInfoByAdmin(selectedTicket._id));
-      dispatch(updateStatusResponse(selectedTicket._id));
-      setMessage("");
+      await getResponseTicketApi(data);
+      dispatch(updateStatusResponse(_id));
     }
   };
 
   return (
     <div>
-      <Form autoComplete="off" onSubmit={handleOnSubmit}>
+      <Form autoComplete="off" onSubmit={handleOnSubmit} name="form">
         <br />
-        <Form.Label>Reponse</Form.Label>
-        <br />
-        {/* texte d'indication */}
-        <Form.Text>
-          Veillez saisir votre reponse dans le champ ci-dessous
-        </Form.Text>
-        {/* champ de saisi du message*/}
-        <Form.Control
-          as="textarea"
-          row="5"
-          name="message"
-          onChange={handleOnChange}
-          value={message}
-        />
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Reponse
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="5"
+            name="message"
+            onChange={handleOnChange}
+            value={message}
+          ></textarea>
+        </div>
+
         {/* bouton de soumission de la reponse */}
-        <div className="text-right mt-3 mb-3">
+        <div className="text-right mt-3 mb-3 d-flex">
           {user.role === "client" ? (
             <Button variant="info" type="submit">
               soumettre
@@ -84,6 +143,15 @@ const UpdateTicket = ({ _id }) => {
               Repondre
             </Button>
           )}
+          <div className="form-group">
+            <input
+              type="file"
+              name="image"
+              className="form-control"
+              id="formFile"
+              onChange={handleFileInput}
+            />
+          </div>
         </div>
       </Form>
     </div>
